3244198a3d0351b383ab04fef9f473b8db1abb69
rdezat <44586851+rdezat@users.noreply.github.com>
2018-10-29 21:04:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from src import scraper_setdArt
+
+# Pagina a investigar
+url = "https://www.setdart.com"
+
+# Busquem categories de subasta
+categorias = scraper_setdArt.busca_categorias(url)
+
+# Busquem lots de cada categoria de subasta 
+lotes = scraper_setdArt.busca_lots(categorias, url)
+       
+# Recollim dades de les pujes         
+pujas = scraper_setdArt.busca_pujas(lotes, url)
+
+# Estandaditzem les dades
+pujas = scraper_setdArt.estandard_datos(pujas)
+
